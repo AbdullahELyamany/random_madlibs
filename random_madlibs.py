d7844aa0a6c9e3f7eb25a24e1ca01dc6f6f91a15
AbdullahELyamany <124623013+AbdullahELyamany@users.noreply.github.com>
2023-06-02 10:13:25 +0300

    Add files via upload

---
+++ random_madlibs.py
@@ -0,0 +1,8 @@
+# Created by Abdullah Al-Yamani
+
+from sample_madlibs import hp, code, zombie, hungergames
+import random
+
+if __name__ == "__main__":
+    m = random.choice([hp, code, zombie, hungergames])
+    m.madlib()
